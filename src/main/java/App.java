e5d5317bbec94bf20f8f30a7ace9af308b6b5026
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-10-11 00:13:38 +0300

    import bufferredReader

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,17 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("I'm a Caesar Cipher application!");
+//    public static void main(String[] args) {
+//        System.out.println("I'm a Caesar Cipher application!");
+public static void main(String[] args) {
+    boolean runApp = true;
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("I'm a Caesar Cipher application!");
+    System.out.println("What would you like to do: 1. Encode 2. Decode 3. Exit (Enter 1, 2 or 3)");
     }
 
 
